647b6a6bbbb8a5c90f95c350379d769f7e6be29d
Sage Skaff <skaffsage@gmail.com>
2022-05-20 14:43:16 -0700

    Add application create route

--- config/routes.rb
+++ config/routes.rb
@@ -39,5 +39,6 @@ Rails.application.routes.draw do
 
   get '/applications/:application_id', to: 'applications#show'
   get '/applications', to: 'applications#index'
-  get '/applications/new', to: 'applications#new' 
+  get '/applications/new', to: 'applications#new'
+  post '/applications', to: 'applications#create'
 end
