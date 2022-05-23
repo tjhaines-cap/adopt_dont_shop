be8d775f7aa5aed1734c683eb4a98251ed502719
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-23 14:11:20 -0600

    Feat: add admin shelters controller

--- config/routes.rb
+++ config/routes.rb
@@ -43,4 +43,5 @@ Rails.application.routes.draw do
   post '/applications', to: 'applications#create'
   patch '/applications/:application_id', to: 'applications#update'
 
+  get '/admin/shelters', to: 'admin_shelters#index'
 end
