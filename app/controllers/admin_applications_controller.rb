ca1a3cfd8ead9ee5dfc3e92df618fd823a290ffd
Sage Skaff <skaffsage@gmail.com>
2022-05-25 14:28:42 -0700

    Add pets_approved

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,5 +1,11 @@
 class AdminApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    @pets_approved = []
+    @pets_approved << params[:pet_id] if params[:pet_id]
+  end
+
+  def approve
+    redirect_to "/admin/applications/#{params[:id]}"
   end
 end
