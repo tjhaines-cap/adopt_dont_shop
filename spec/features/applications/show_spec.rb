0a6de469a88bd6004ea3a2174c7b881069052c4c
Sage Skaff <skaffsage@gmail.com>
2022-05-25 09:33:49 -0700

    Add application submit test

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -33,61 +33,85 @@ RSpec.describe 'Applications Show Page' do
   end
 
   it 'has section to search for pet on in progress application' do
-    application = Application.create!(name: 'Jenn', address: '123 Water Street, Denver, CO, 80111', description: 'I like animals!', status: 'In Progress')
-    application2 = Application.create!(name: 'Sage', address: '42 Wind Avenue, Denver, CO, 80111', description: 'Happy home', status: 'Pending')
+    application = Application.create!(name: 'Jenn', address: '123 Water Street, Denver, CO, 80111',
+                                      description: 'I like animals!', status: 'In Progress')
+    application2 = Application.create!(name: 'Sage', address: '42 Wind Avenue, Denver, CO, 80111',
+                                       description: 'Happy home', status: 'Pending')
     shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
     pet_3 = Pet.create(adoptable: true, age: 2, breed: 'labrador', name: 'Lobster', shelter_id: shelter.id)
     pet_4 = Pet.create(adoptable: true, age: 1, breed: 'chihuahua', name: 'Tiny', shelter_id: shelter.id)
 
     visit "/applications/#{application.id}"
-    expect(page).to have_content("Add a Pet to this Application")
-    
-    fill_in 'search', with: pet_2.name 
-    click_button("Find Pets")
+    expect(page).to have_content('Add a Pet to this Application')
+
+    fill_in 'search', with: pet_2.name
+    click_button('Find Pets')
     expect(current_path).to eq("/applications/#{application.id}")
-    
+
     expect(page).to have_content(pet_2.name)
     expect(page).to have_content(pet_2.age)
     expect(page).to have_content(pet_2.adoptable)
     expect(page).to have_content(pet_2.breed)
     expect(page).to have_content(pet_2.shelter_name)
-    
+
     expect(page).to have_content(pet_3.name)
     expect(page).to have_content(pet_3.age)
     expect(page).to have_content(pet_3.adoptable)
     expect(page).to have_content(pet_3.breed)
     expect(page).to have_content(pet_3.shelter_name)
-    
-    expect(page).to have_content("Add a Pet to this Application")
-    fill_in 'search', with: "Max" 
-    click_button("Find Pets")
-    expect(page).to_not have_content("Max")
+
+    expect(page).to have_content('Add a Pet to this Application')
+    fill_in 'search', with: 'Max'
+    click_button('Find Pets')
+    expect(page).to_not have_content('Max')
     expect(page).to_not have_content(pet_2.name)
     expect(page).to_not have_content(pet_3.name)
     expect(page).to_not have_content(pet_4.name)
-    
+
     expect(current_path).to eq("/applications/#{application.id}")
-  
+
     visit "/applications/#{application2.id}"
-    expect(page).to_not have_content("Add a Pet to this Application")
+    expect(page).to_not have_content('Add a Pet to this Application')
   end
 
-  it 'can add pet to application from list of pets that appears after a namee is searched' do
-    application = Application.create!(name: 'Jenn', address: '123 Water Street, Denver, CO, 80111', description: 'I like animals!', status: 'In Progress')
+  it 'can add pet to application from list of pets that appears after a name is searched' do
+    application = Application.create!(name: 'Jenn', address: '123 Water Street, Denver, CO, 80111',
+                                      description: 'I like animals!', status: 'In Progress')
     shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
     pet_3 = Pet.create(adoptable: true, age: 2, breed: 'labrador', name: 'Lobster', shelter_id: shelter.id)
     pet_4 = Pet.create(adoptable: true, age: 1, breed: 'chihuahua', name: 'Tiny', shelter_id: shelter.id)
 
     visit "/applications/#{application.id}"
-    expect(page).to_not have_content("Lobster")
-    fill_in 'search', with: pet_2.name 
-    click_button("Find Pets")
+    expect(page).to_not have_content('Lobster')
+    fill_in 'search', with: pet_2.name
+    click_button('Find Pets')
     expect(current_path).to eq("/applications/#{application.id}")
     click_button(pet_3.id)
 
     expect(current_path).to eq("/applications/#{application.id}")
-    expect(page).to have_content("Lobster")
+    expect(page).to have_content('Lobster')
+  end
+
+  it 'can submit application after a pet has been added' do
+    application = Application.create!(name: 'Jenn', address: '123 Water Street, Denver, CO, 80111',
+                                      description: 'I like animals!', status: 'In Progress')
+    shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Leon', shelter_id: shelter.id)
+    pet_3 = Pet.create(adoptable: true, age: 2, breed: 'labrador', name: 'Lobster', shelter_id: shelter.id)
+    pet_4 = Pet.create(adoptable: true, age: 1, breed: 'chihuahua', name: 'Tiny', shelter_id: shelter.id)
+
+    visit "/applications/#{application.id}"
+    expect(current_path).to eq("/applications/#{application.id}")
+    click_button(pet_3.id)
+
+    fill_in 'description', with: 'Lots of love!'
+    click_button('submit')
+    expect(current_path).to eq("/applications/#{application.id}")
+    expect(page).to have_content('Pending')
+    expect(page).to have_content('Lots of love!')
+    expect(page).to have_content('Lobster')
+    expect(page).to_not have_content('Add a Pet to this Application')
   end
 end
