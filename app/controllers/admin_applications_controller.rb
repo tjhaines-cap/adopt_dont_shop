146e1082cc870c527dcb544c671e40e7f530a8b8
Sage Skaff <skaffsage@gmail.com>
2022-05-25 15:04:03 -0700

    Update show method

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,9 +1,12 @@
 class AdminApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    @pets_approved = []
-    @pets_approved << params[:pet_id] if params[:pet_id]
     @pets_rejected = []
-    @pets_rejected << params[:pet_id] if params[:pet_id]
+    @pets_approved = []
+    if params[:approved] == 'true'
+      @pets_approved << params[:pet_id] if params[:pet_id]
+    elsif params[:approved] == 'false'
+      @pets_rejected << params[:pet_id] if params[:pet_id]
+    end
   end
 end
